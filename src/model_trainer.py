7d9674a54ee4e5f661b5d0048259357ea5b308e7
DTI-dream <118739923+DTI-dream@users.noreply.github.com>
2023-04-09 13:26:15 +0800

    Update model_trainer.py

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -25,9 +25,6 @@ class ModelTrainer(object):
             label_list.extend(labels.tolist())
 
             # inputs, labels = data
-            # 这里需要将pairs_indexes转化成torch.tensor形式
-            # 第一个是protein 400,第二个是durg 100
-            # 我试一下分开传入
             pairs = pairs_indexes[0]
             proteins_feature, drugs_feature = pairs
             proteins_feature = proteins_feature.to(device)
@@ -41,25 +38,20 @@ class ModelTrainer(object):
             # inputs = pairs_indexes_list_tensor.to(device)
             labels = labels.long().to(device)
             #labels = labels.unsqueeze(-1)
-            # forward & backward
             outputs = model(proteins_feature, drugs_feature, indexes)
             optimizer.zero_grad()
-            # 计算auc
             outputs_class = np.argmax(outputs.cpu().detach().numpy(), axis=1)
             auc_value = roc_auc_score(labels.cpu().detach().numpy(), outputs_class)
-            # 计算f1
             f1_train = f1_score(labels.cpu().detach().numpy(), outputs_class)
 
             loss = loss_f(outputs.cpu(), labels.cpu())
             loss.backward()
             optimizer.step()
 
-            # 统计loss
             loss_sigma.append(loss.item())
             loss_mean = np.mean(loss_sigma)
 
             true_list = []
-            # 统计混淆矩阵
             _, predicted = torch.max(outputs.data, 1)
             for j in range(len(labels)):
                 cate_i = labels[j].cpu().numpy()
@@ -71,12 +63,10 @@ class ModelTrainer(object):
                 if cate_i != pre_i:
                     path_error.append((cate_i, pre_i))  # 记录错误样本的信息
 
-            # 统计loss
             loss_sigma.append(loss.item())
 
             acc_avg = len(true_list) / len(labels)
 
-            # 每10个iteration 打印一次训练信息
             if i % cfg.log_interval == cfg.log_interval - 1:
                 logger.info("Training: Epoch[{:0>3}/{:0>3}] Iteration[{:0>3}/{:0>3}] Loss: {:.4f} Acc:{:.2%} train_auc:{:.2%} f1:{:.2%}".
                             format(epoch_idx + 1, cfg.max_epoch, i + 1, len(data_loader), loss_mean, acc_avg, auc_value, f1_train))
@@ -98,10 +88,7 @@ class ModelTrainer(object):
             # label_list.extend(labels.tolist())
 
             # inputs, labels = data
-            # 这里需要将pairs_indexes转化成torch.tensor形式
-            # 第一个是protein 400,第二个是durg 100
-            # 我试一下分开传入
-            pairs = pairs_indexes[0]
+             pairs = pairs_indexes[0]
             proteins_feature, drugs_feature = pairs
             proteins_feature = proteins_feature.to(device)
             drugs_feature = drugs_feature.to(device)
@@ -116,16 +103,13 @@ class ModelTrainer(object):
 
             outputs = model(proteins_feature, drugs_feature, indexes)
             outputs_class = np.argmax(outputs.cpu().detach().numpy(), axis=1)
-            # 计算测试的时候的auc
             auc_value = roc_auc_score(labels.cpu().detach().numpy(), outputs_class)
-            # f1
             f1_valid = f1_score(labels.cpu().detach().numpy(), outputs_class)
 
 
             loss = loss_f(outputs.cpu(), labels.cpu())
 
             true_list = []
-            # 统计混淆矩阵
             _, predicted = torch.max(outputs.data, 1)
             for j in range(len(labels)):
                 cate_i = labels[j].cpu().numpy()
@@ -137,7 +121,6 @@ class ModelTrainer(object):
                 if cate_i != pre_i:
                     path_error.append((cate_i, pre_i))  # 记录错误样本的信息
 
-            # 统计loss
             loss_sigma.append(loss.item())
 
             acc_avg = len(true_list) / len(labels)
